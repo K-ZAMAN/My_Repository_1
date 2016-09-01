23d93a0bd9ca0043ffa82fb4785acd495e7da8c7
K-ZAMAN <khosruzzzaman@gmail.com>
2016-08-31 23:11:27 -0400

    Added another Variable to find the second value

--- AllHomeWork/src/Practies/DateTime.java
+++ AllHomeWork/src/Practies/DateTime.java
@@ -4,9 +4,11 @@ package Practies;
 public class DateTime {
 
 	public static void main(String[] args) {
-	int a = 20;
-	int b = 30;
-	System.out.println("The Highest value is : "+Math.max(a, b));
+	int a = 2;
+	int b = 3;
+	int c=222;
+	System.out.println("The Highest value is : "+(Math.min(Math.min(a, b),c)));
+	
 		
 	}
 }
